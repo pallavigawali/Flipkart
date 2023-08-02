f82a85e714260c3cdf7ba86ce8474ad419928ac9
pallavigawali <gawalipallavi20@gmail.com>
2023-08-02 12:52:24 +0530

    [pallavi]New Changes

--- src/test/java/demo/Flipkart.java
+++ src/test/java/demo/Flipkart.java
@@ -6,12 +6,14 @@ import org.testng.annotations.Test;
 
 public class Flipkart {
   @Test
-  public void flipkart1() 
+  public void flipkart1() throws InterruptedException 
   {
 	    ChromeOptions option=new ChromeOptions();
 		option.addArguments("--remote-allow-origins=*");
 		WebDriver driver=new ChromeDriver(option);
 		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com/");
+		Thread.sleep(1000);
+		System.out.println("This is first change");
   }
 }
