e02b7bc58039ce336d5f90d0ab218c470faf0490
pallavigawali <gawalipallavi20@gmail.com>
2023-08-02 12:12:12 +0530

    [Pallavi]Its my 1st Test case

---
+++ src/test/java/demo/Amazon.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Amazon {
+
+  @Test
+  public void flipkart1Test() 
+  {
+	  ChromeOptions option=new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(option);
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+
+  }
+}
---
+++ src/test/java/demo/Flipkart.java
@@ -0,0 +1,17 @@
+package demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  public void flipkart1() 
+  {
+	    ChromeOptions option=new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(option);
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+  }
+}
